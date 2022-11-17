285dcf57b0019602b4344ec9760726539e1bab53
hp <hp@Sudipta>
2022-11-17 11:52:08 +0530

    Commit new class

---
+++ src/test/java/testRunner/SampleClass.java
@@ -0,0 +1,14 @@
+package testRunner;
+
+public class SampleClass {
+	public void DemoClass()
+	{
+		System.out.println("Demo class");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
